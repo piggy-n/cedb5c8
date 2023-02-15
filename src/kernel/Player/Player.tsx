7222f2f54eb45314d56ea695a93aad09d67cb27f
lzn01 <408135242@qq.com>
2023-02-15 14:28:01 +0800

    uuid

--- src/kernel/Player/Player.tsx
+++ src/kernel/Player/Player.tsx
@@ -1,8 +1,9 @@
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 import s from './styles/player.scss';
 import c from 'classnames';
 import type { ForwardRefRenderFunction } from 'react';
 import type { PlayerRef, PlayerProps } from '@/index.d';
+import { randomString } from '@/utils/methods/randomString';
 
 const VanillaPlayer: ForwardRefRenderFunction<PlayerRef, PlayerProps> = (
     {
@@ -11,8 +12,11 @@ const VanillaPlayer: ForwardRefRenderFunction<PlayerRef, PlayerProps> = (
     },
     ref
 ) => {
+    const [uuid] = useState(randomString());
+
     return (
         <div
+            id={`player-${uuid}`}
             {...videoContainerEleOpts}
             className={c(s.container, videoContainerEleOpts?.className)}
         />
---
+++ src/utils/methods/randomString.ts
@@ -0,0 +1,6 @@
+export const randomString = (length = 8) => {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+};
