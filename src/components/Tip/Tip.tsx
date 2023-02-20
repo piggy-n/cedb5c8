a8fcaf6b7cfab043cefe03791a0c160c39964344
lzn01 <408135242@qq.com>
2023-02-20 15:00:45 +0800

    Tip 组件

---
+++ src/components/Tip/Tip.tsx
@@ -0,0 +1,87 @@
+import type { FC } from 'react';
+import c from 'classnames';
+import s from './styles/tip.scss';
+import { useEffect, useRef } from 'react';
+import { createRoot } from 'react-dom/client';
+
+interface TipProps {
+    msg: string;
+    eleId: string;
+    uuid: string;
+    type?: 'success' | 'error' | 'warning' | 'info';
+}
+
+const Tip: FC<TipProps> = (
+    {
+        msg,
+        eleId,
+        uuid,
+        type = 'info',
+    },
+) => {
+    const tipTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+    const removeHandler = () => {
+        if (document.querySelector(`#tip-${uuid}`)) {
+            document
+                .querySelector(`#${eleId}-${uuid}`)!
+                .removeChild(document.querySelector(`#tip-${uuid}`)!);
+        }
+    };
+
+    useEffect(
+        () => {
+            tipTimeoutRef.current = setTimeout(
+                () => removeHandler(),
+                3000,
+            );
+
+            return () => {
+                tipTimeoutRef.current && clearTimeout(tipTimeoutRef.current);
+            };
+        },
+        [],
+    );
+
+    return (
+        <div className={c(s.container, s[type])}>
+            {msg}
+        </div>
+    );
+};
+
+const crateTip = (option: TipProps) => {
+    const { eleId, uuid } = option;
+    const tip = document.querySelector(`#tip-${uuid}`);
+
+    if (!tip) {
+        const container = document.createElement('div');
+        const tipWrapperEle = document.querySelector(`#${eleId}-${uuid}`);
+        container.id = `tip-${uuid}`;
+
+        if (tipWrapperEle) {
+            tipWrapperEle.appendChild(container);
+            const root = createRoot(container);
+            root.render(<Tip {...option} />);
+        }
+    }
+};
+
+const removeTip = (option: { uuid: string; eleId: string; }) => {
+    const { eleId, uuid } = option;
+    const tip = document.querySelector(`#tip-${uuid}`);
+
+    if (tip) {
+        const tipWrapperEle = document.querySelector(`#${eleId}-${uuid}`);
+
+        if (tipWrapperEle) {
+            tipWrapperEle.removeChild(tip);
+        }
+    }
+};
+
+export const tip = (option: TipProps) => {
+    const { eleId, uuid } = option;
+    removeTip({ eleId, uuid });
+    crateTip(option);
+};
---
+++ src/components/Tip/index.ts
@@ -0,0 +1,3 @@
+import { tip } from './Tip';
+
+export { tip };
