171d282d532188d94484c593b239bc69ba533b45
lzn01 <408135242@qq.com>
2023-02-22 11:29:27 +0800

    修改文件夹名称

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -8,8 +8,8 @@ import useVideoResizing from '@/utils/hooks/video/useVideoResizing';
 import useVideoPlayer from '@/utils/hooks/video/useVideoPlayer';
 import useVideoLoading from '@/utils/hooks/video/useVideoLoading';
 import useControlsAutoHidden from '@/utils/hooks/controls/useControlsAutoHidden';
-import usePlayerMethods from '@/utils/hooks/method/usePlayerMethods';
-import useControlsMethods from '@/utils/hooks/method/useControlsMethods';
+import usePlayerMethods from '@/utils/hooks/methods/usePlayerMethods';
+import useControlsMethods from '@/utils/hooks/methods/useControlsMethods';
 
 export {
     useMandatoryUpdate,
