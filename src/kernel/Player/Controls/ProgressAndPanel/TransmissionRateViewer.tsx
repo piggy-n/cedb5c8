63516c1bd94cb6ecbb0990ba0a5f59e4117b6e52
lzn01 <408135242@qq.com>
2023-02-27 09:32:06 +0800

    TransmissionRateViewer

--- src/kernel/Player/Controls/ProgressAndPanel/TransmissionRateViewer.tsx
+++ src/kernel/Player/Controls/ProgressAndPanel/TransmissionRateViewer.tsx
@@ -1,6 +1,27 @@
+import s from './styles/transmissionRateViewer.scss';
+import { useContext, useMemo } from 'react';
+import { PlayerContext } from '@/utils/hooks/data/usePlayerContext';
+
 const TransmissionRateViewer = () => {
+    const {
+        playerStore: {
+            transmissionRate = 0,
+            videoType,
+        },
+    } = useContext(PlayerContext);
+
+    const rate = useMemo(
+        () => transmissionRate >= 1024
+            ? `${(transmissionRate / 1024).toFixed(2)}Mbps`
+            : `${transmissionRate.toFixed(2)}Kbps`,
+        [transmissionRate],
+    );
+
+    if (videoType === 'record') return null;
     return (
-        <></>
+        <div className={s.container}>
+            {rate}
+        </div>
     );
 };
 
