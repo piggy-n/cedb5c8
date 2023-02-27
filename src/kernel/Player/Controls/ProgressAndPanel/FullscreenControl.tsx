916e9a08344f6c0fee0f9f3092651d55b8dda169
lzn01 <408135242@qq.com>
2023-02-27 09:39:44 +0800

    FullscreenControl

--- src/kernel/Player/Controls/ProgressAndPanel/FullscreenControl.tsx
+++ src/kernel/Player/Controls/ProgressAndPanel/FullscreenControl.tsx
@@ -1,6 +1,29 @@
+import s from './styles/fullscreenControl.scss';
+import { useContext } from 'react';
+import { PlayerContext } from '@/utils/hooks/data/usePlayerContext';
+import { isBoolean } from 'ahooks/es/utils';
+import { fullscreen } from '@/utils/methods/common/fullscreen';
+
 const FullscreenControl = () => {
+    const {
+        controlsOpts,
+        videoContainerEle,
+        playerStoreDispatch,
+    } = useContext(PlayerContext);
+
+    const clickHandler = async () => {
+        const isFullscreen = await fullscreen(videoContainerEle);
+
+        playerStoreDispatch({
+            isFullscreen,
+        });
+    };
+
+    if ((isBoolean(controlsOpts) && !controlsOpts) || !controlsOpts?.fullscreen) return null;
     return (
-        <></>
+        <div className={s.container} onClick={clickHandler}>
+            全
+        </div>
     );
 };
 
