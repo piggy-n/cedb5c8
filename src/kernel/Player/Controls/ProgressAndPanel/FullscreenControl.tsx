ccfba37dd898f65b0cdf87893c5a3f3bea57a27e
lzn01 <408135242@qq.com>
2023-02-27 16:10:47 +0800

    全屏 icon

--- src/kernel/Player/Controls/ProgressAndPanel/FullscreenControl.tsx
+++ src/kernel/Player/Controls/ProgressAndPanel/FullscreenControl.tsx
@@ -3,12 +3,16 @@ import { useContext } from 'react';
 import { PlayerContext } from '@/utils/hooks/data/usePlayerContext';
 import { isBoolean, isObject } from 'ahooks/es/utils';
 import { fullscreen } from '@/utils/methods/common/fullscreen';
+import Icon from '@/components/Icon';
 
 const FullscreenControl = () => {
     const {
         controlsOpts,
         videoContainerEle,
         playerStoreDispatch,
+        playerStore: {
+            isFullscreen,
+        },
     } = useContext(PlayerContext);
 
     const clickHandler = async () => {
@@ -26,7 +30,11 @@ const FullscreenControl = () => {
     ) return null;
     return (
         <div className={s.container} onClick={clickHandler}>
-            全
+            <Icon
+                name={isFullscreen ? 'fullscreen-2' : 'fullscreen-1'}
+                size={18}
+                title={isFullscreen ? '退出全屏' : '全屏'}
+            />
         </div>
     );
 };
